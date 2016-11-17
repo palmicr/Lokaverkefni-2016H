19c6be17dfbc077f18bab723edee1fc2b88080d0
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2016-11-17 12:10:35 +0000

    SET UP FOR GIT

---
+++ Ball_Launcher_1.0.c
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------------------------------*\
+|*                             	       - Lokaverkefni Vetur 2016 -                                    *|
+|*                                           Ball_Launcher
+|*                                      ROBOTC on VEX 2.0 CORTEX                                      *|
+|*                                                                                                    *|
+|*                                        ROBOT CONFIGURATION                                         *|
+|*    NOTES:                                                                                          *|
+|*                                                                                                    *|
+|*    MOTORS & SENSORS:                                                                               *|
+|*    [I/O Port]          [Name]              [Type]                [Description]                     *|
+
+\*-----------------------------------------------------------------------------------------------4246-*/
+task main()
+{
+	bool Run = false;
+
+	if(Run == false)
+	{
+		StandBy();
+	}
+	else if(Run == true)
+	{
+		//Take Aim
+	}
+}
